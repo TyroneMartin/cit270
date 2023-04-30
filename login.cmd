a1e36abb1326ef423f44a4ec5f7bc2858875014c
Tyrone Martin <tyrone306@edu.com>
2023-04-29 21:54:46 -0600

    link

--- login.cmd
+++ login.cmd
@@ -1,2 +1,7 @@
 echo "Logging in"
 
+@REM  curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
+
+curl https://dev.stedi.me/validate/781942c6-934b-4a9c-88d1-a93dfea4079b
+
+
