b58a3c3c1e14f6195d16b2441ffbe31132a04711
Tyrone Martin <tyrone306@edu.com>
2023-05-13 14:51:56 -0600

    update 5/138/23

--- login.cmd
+++ login.cmd
@@ -1,7 +1,9 @@
 echo "Logging in"
 
+curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" http://localhost:3000/login
+
 @REM  curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
 
-curl https://dev.stedi.me/validate/781942c6-934b-4a9c-88d1-a93dfea4079b
+@REM curl https://dev.stedi.me/validate/781942c6-934b-4a9c-88d1-a93dfea4079b
 
 
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express= require('express');
+const bodyParser = require('body-parser')
+const Redis = require ('redis');
 const app=express();
 
 const port = 3000;
+const redisClient = Redis.createClient();
+
+app.use(bodyParser.json ());//allow json (Javascript object Notation) requests
+
 app.listen(port, ()=> {
+    redisClient.connect();
 console.log("listening on port: " + port)
 });
 
@@ -11,4 +18,17 @@ app.get("/", (req, res) => {
     // res.resirect ("https:google.com")
 })
 
-
+app.post('/login', (req, res) => {
+    const loginBody =req.body;
+    const userName = loginBody.userName;
+    const password = loginBody.password
+    if (password==="mudsaD58@@") {
+        //this happens if the password is correct
+        res.send ("Welcome" + userName)
+    } else {
+        //this happens if the password is not correct
+        res.status (401); //unauthorized
+        res.send("Incorrect password");
+    }
+    res.send("Welcome"+ userName);
+});
