f7656f15fa8517b1f42360ad26d084eac67dc6f4
Tyrone Martin <tyrone306@edu.com>
2023-05-19 20:23:27 -0600

    update 5/19/23

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express= require('express');
 const bodyParser = require('body-parser')
 const Redis = require ('redis');
 const app=express();
-
+const { createHash } =  require('node:crypto')
 const port = 3000;
-const redisClient = Redis.createClient();
+const redisClient = Redis.createClient({url:'redis://127.0.0.1:6379'});
 
 app.use(bodyParser.json ());//allow json (Javascript object Notation) requests
 
@@ -18,11 +18,16 @@ app.get("/", (req, res) => {
     // res.resirect ("https:google.com")
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const loginBody =req.body;
     const userName = loginBody.userName;
-    const password = loginBody.password
-    if (password==="mudsaD58@@") {
+    const password = loginBody.password; //we need to hash the password the user gave us
+    const hashedPassword = password==null? null : createHash('sha3-256').update(password).digest('hex');
+    console.log("Hashed Password: " + hashedPassword);
+    const redisPassword = password==null ? null : await redisClient.hGet('hashedpasswords',userName);
+    console.log("Passord for " + userName + " " + redisPassword);
+   
+    if (password!= null && hashedPassword===redisPassword) {
         //this happens if the password is correct
         res.send ("Welcome" + userName)
     } else {
@@ -30,5 +35,4 @@ app.post('/login', (req, res) => {
         res.status (401); //unauthorized
         res.send("Incorrect password");
     }
-    res.send("Welcome"+ userName);
 });
